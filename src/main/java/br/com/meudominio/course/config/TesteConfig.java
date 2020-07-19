fe890d6b176202848955a58e7e2004a85c59cc0f
Leandro Bove <leandrobove@hotmail.com>
2020-07-19 17:39:10 -0300

    Criação e mapeamento de entidades User e Order com relacionamento.

--- src/main/java/br/com/meudominio/course/config/TesteConfig.java
+++ src/main/java/br/com/meudominio/course/config/TesteConfig.java
@@ -1,5 +1,6 @@
 package br.com.meudominio.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.meudominio.course.entities.Order;
 import br.com.meudominio.course.entities.User;
 import br.com.meudominio.course.repositories.UserRepository;
 
@@ -22,6 +24,10 @@ public class TesteConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 	}
--- src/main/java/br/com/meudominio/course/repositories/UserRepository.java
+++ src/main/java/br/com/meudominio/course/repositories/UserRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.meudominio.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
 	
 }
