c9b96751e3085bc076e645797d752681c6c12839
Leandro Bove <leandrobove@hotmail.com>
2020-07-19 16:10:04 -0300

    User Entity e Resource

---
+++ src/main/java/br/com/meudominio/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package br.com.meudominio.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.meudominio.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "845456564", "12345");
+
+		return ResponseEntity.ok().body(u);
+	}
+
+}
