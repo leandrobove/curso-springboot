d4135d80695dd05da549732d7eedb1897c6bc94a
Leandro Bove <leandrobove@hotmail.com>
2020-07-23 14:09:55 -0300

    Exception handling - Delete User

--- src/main/java/br/com/meudominio/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/meudominio/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.meudominio.course.services.exceptions.DataBaseException;
 import br.com.meudominio.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,6 +21,19 @@ public class ResourceExceptionHandler {
 		String error = "Resource not found.";
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				req.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest req) {
+
+		String error = "Database error.";
+
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				req.getRequestURI());
